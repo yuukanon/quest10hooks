6cec3e21c985227f2c521094141b5f6db00909aa
Yuuka Motobayashi <yuukanon@gmail.com>
2020-07-21 17:30:04 +0200

    counter hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const Counter = () => {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>The counter is at : {count}</p>
+      <button onClick={() => setCount(count+1)}>+1</button>
+      <button onClick={() => setCount(count-1)}>-1</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default Counter;
